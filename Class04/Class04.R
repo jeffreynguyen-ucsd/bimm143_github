44295cfa2b0d969a85d63fa73b4b1b8d3ad516fe
Jeffrey Nguyen <jpn008@ucsd.edu>
2025-05-20 12:04:28 -0700

    Class 4

---
+++ Class04/Class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Jeffrey Nguyen A16776318"
+#' date: "April 10th, 2025"
+#' ---
+# My first R script
+x <- 1:50
+plot(x,sin(x))
+
+plot(x,sin(x),typ="l", col="blue", lwd=3,
+     xlab="Silly x axis",ylab="Sensible y axis")
---
+++ Class04/Graph.R
@@ -0,0 +1,6 @@
+# My first R script
+x <- 1:50
+plot(x,sin(x))
+
+plot(x,sin(x),typ="l", col="blue", lwd=3,
+     xlab="Silly x axis",ylab="Sensible y axis")
